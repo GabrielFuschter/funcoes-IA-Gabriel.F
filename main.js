6bea2064e2fad3c2ede7b8155c79b58ee937d812
GabrielFuschter <108005423+GabrielFuschter@users.noreply.github.com>
2024-08-16 10:51:23 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-
+const caixaPrincipal = document.querySelectorAll(".caixa-principal");
+const caixaPerguntas = document.querySelectorAll(".caixa-perguntas");
+const caixaAlternativas = document.querySelectorAll(".caixa-alternativas");
+const caixaResultado = document.querySelectorAll(".caixa-resultado");
 
 
 
